d9146bb90718fd6e3b9212be96477d0293d3ee81
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-18 16:12:15 +0300

    Kapsulleme-Ornek
    Kapsulleme-Ornek

---
+++ ConsoleAppKapsulleme/Ogrenci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsulleme
+{
+    class Ogrenci
+    {
+        /*  
+         İŞE PUBLİC TÜRÜNDEN DEĞİŞKEN TANIMLAYARAK BAŞLADIM
+         BU TANIMLAYIP DEĞER ATADIĞIM DEĞİŞKENİ PROGRAM.CS ÜZERİNDEN WRITELINLE SORUNSUZ YAZDIRDIM
+         ANCAK PUBLİC STRİNG TURUNDEN OLAN BU DEĞİŞKENİMİ PRİVATE YAPTIGIM ANDA ERİŞİMİM ENGELLENDİ
+         AMA KULLANDIGIM IKI METODLA BEN BU DEĞİŞKENİME ULAŞMAYI BAŞARDIM 
+         YANI KAPSÜLLEME YAPTIM
+         */
+
+
+        private string isim;
+
+        //BAHSETTİĞİM IKI METODU OLUŞTURUYRUM 
+        // SET DEĞER ATAMAK 
+        //GET İSE BU DEĞERİ ALMAK İÇİN KULLANDIĞIM METODLAR OLUYOR
+
+        public void setIsim(string _isim)
+        {   
+            //isim karmaşası olmaması adına _ simgesini kullandım
+            isim = _isim;
+        }
+
+
+        public string getIsim()
+        {
+            //değerimi döndürdüm 
+            return isim;
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleAppKapsulleme/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsulleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ogrencı clasımdan nesne türettim
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            // bun nesneye ahmet adını atayabılmek ıcın set ve get methodlarımı cagırıyorum 
+            ogrenci1.setIsim("AHMET");
+             // if blokları içinde rahat kullanabılmem içinde getı atama ıslemını strıng bır degıskene yapıyorum.
+            string gelen = ogrenci1.getIsim();
+            // son olarakda aldıgım ısmın aynı olup olmadıgına bakıyorum (buyuk kucuk harf duy.var)
+            if(gelen == "AHMET")
+            {
+                // ve sonuca görede mesaj yazdırıyorum 
+                Console.WriteLine("BAŞARILI");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("BAŞARISIZ");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
